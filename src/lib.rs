422940df7509baf84042cbe49e26c618ee33cb6f
John Schock <joschock@microsoft.com>
2021-08-29 23:04:59 -0700

    Add IDT and breakpoint exception handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -8,7 +9,16 @@ use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
+
+pub fn init() {
+    interrupts::init_idt();
+}
+
+//
+// Test Support
+//
 pub trait Testable {
     fn run(&self) -> ();
 }
@@ -43,6 +53,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
