39150c0c4af21c5295dd7713262f58abee6faae7
John Schock <joschock@microsoft.com>
2021-08-30 22:34:34 -0700

    Add PIC support and timer interrupt handler 	Disable interrupts in print routines to avoid deadlock 	Address race condition in test_println_output() 	Add halt loop for idle 	Add keyboard scancode decode

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,14 @@ pub mod gdt;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    interrupts::init_pics();
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 //
@@ -48,7 +56,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 /// Entry point for `cargo test`
@@ -57,7 +65,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
