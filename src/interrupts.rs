c7baebece3c24c94e30e4d9ce155fe584a9c5f80
John Schock <joschock@microsoft.com>
2021-08-31 20:24:55 -0700

    Add page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,6 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{
+  InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode
+};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
@@ -6,6 +8,7 @@ use spin;
 use crate::print;
 use crate::println;
 use crate::gdt;
+use crate::hlt_loop;
 
 lazy_static! {
   static ref IDT: InterruptDescriptorTable = {
@@ -15,6 +18,7 @@ lazy_static! {
       idt.double_fault.set_handler_fn(double_fault_handler)
         .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
     }
+    idt.page_fault.set_handler_fn(page_fault_handler);
     idt[InterruptIndex::Timer.as_usize()]
       .set_handler_fn(timer_interrupt_handler);
     idt[InterruptIndex::Keyboard.as_usize()]
@@ -41,6 +45,18 @@ extern "x86-interrupt" fn double_fault_handler(
   panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn page_fault_handler(
+  stack_frame: InterruptStackFrame,
+  error_code: PageFaultErrorCode
+) {
+  use x86_64::registers::control::Cr2;
+
+  println!("EXCEPTION: PAGE FAULT");
+  println!("Accessed Address: {:?}", Cr2::read());
+  println!("Error Code: {:?}", error_code);
+  println!("{:#?}", stack_frame);
+  hlt_loop();
+}
 
 //
 // PIC interrupts
