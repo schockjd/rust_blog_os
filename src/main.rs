51ee26fd5d022555776520017a5a0da86bf566a3
John Schock <joschock@microsoft.com>
2021-11-21 20:05:04 -0800

    Add task support using await/async

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ extern crate alloc;
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 use blog_os::println;
-
-use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use blog_os::task::{Task, executor::Executor, keyboard};
 
 entry_point!(kernel_main);
 
@@ -32,28 +31,26 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap init failed");
 
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
 
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
     blog_os::hlt_loop();
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
 
 /// This function is called on panic.
